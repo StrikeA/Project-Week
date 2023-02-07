444c5effdfbf6ed50f7364a97a9db4dc057560e0
MasonSeaquist2 <122318779+MasonSeaquist2@users.noreply.github.com>
2023-02-07 12:38:39 -0600

    Better formula

--- ASRScalc.py
+++ ASRScalc.py
@@ -1,10 +1,12 @@
+games = int(input("How many games did they play"))
 minpg = float(input("How many minutes per game"))
 wsper48 = float(input("How many wins per 48"))
 teamsrsabs = float(input("WHat is the absolute value of team srs"))
+teamsrsabs2 = teamsrsabs
 if teamsrsabs < 1:
-    teamsrsabs = 1
+    teamsrsabs2 = 1
 
-playervalue = (((minpg/48) * wsper48) * teamsrsabs)
+playervalue = ((((minpg/48) * wsper48) * teamsrsabs2)/(games/82))
 print(playervalue)
 print("if team srs is negative:")
 print(playervalue + teamsrsabs)

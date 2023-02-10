763acc72aa732ea41a3fa41c58734d9acf5e36ec
Mason Seaquist <mason.seaquist@headwaters.org>
2023-02-10 11:45:23 -0600

    Update WebScraper.py

--- WebScraper.py
+++ WebScraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Away_team = input("Input team abbreviation: ")
+Away_team = "TOR"
 url = "https://www.basketball-reference.com/boxscores/202202030TOR.html"
 
 result = requests.get(url)
@@ -10,38 +12,31 @@ Inactive_Players = doc.find_all(text="Inactive: ")
 
 parent = Inactive_Players[0].parent
 parents = parent.parent
-# print(parents)
-
 players = parents.find_all("a")
-teams = parents.find_all("span")
-'''
-team1 = teams[0].contents
-team2 = teams[1].contents
-team1_text = team1[0]
-team2_text = team2[0]
-strT1 = str(team1_text)
-strT2 = str(team2_text)
-team1_text_text = strT1[8:11]
-team2_text_text = strT2[8:11]
-'''
+parents = str(parents)
 
+teams = parents.split(sep=Away_team)
+T1_players = teams[0]
+T2_players = teams[1]
 
-print(players)
+T1_link = T1_players.split(sep='"')
+
+print(T1_link)
+print(T2_players)
+
 for i in range(len(players)):
     print(i)
+
+    '''
     STRplayer = str(players[i])
     link = STRplayer.split(sep='"')
     link = link[1]
     players_names = STRplayer.split(sep=">")
     players_names = players_names[1]
-    '''
-    print(link[1])
-    print(players_names[:-3])
-    print(STRplayer)
-    '''
+    players_names = players_names[:-3]
+
     player_url = f"https://www.basketball-reference.com{link}"
 
     result1 = requests.get(player_url)
     doc1 = BeautifulSoup(result1.text, "html.parser")
-
-    
+    '''

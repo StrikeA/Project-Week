486562da487d6cdd92061e0fcc8470213ac809be
Mason Seaquist <107698979+StrikeA@users.noreply.github.com>
2023-03-03 14:47:24 -0600

    update

--- ASRScalc.py
+++ ASRScalc.py
@@ -1,14 +1,15 @@
-games = int(input("How many games did they play? "))
-minpg = float(input("How many minutes per game? "))
-wsper48 = float(input("How many wins per 48? "))
-teamsrsabs = float(input("WHat is the absolute value of team srs? "))
+games = int(input("How many games did they play"))
+ofe = int(input("of how many"))
+minpg = float(input("How many minutes per game"))
+wsper48 = float(input("How many wins per 48"))
+teamsrsabs = float(input("WHat is the absolute value of team srs"))
 teamsrsabs2 = teamsrsabs
 if teamsrsabs < 1:
     teamsrsabs2 = 1
 
-playervalue = ((((minpg/48) * wsper48) * teamsrsabs2)/(games/82))
+playervalue = ((((minpg/48) * wsper48) * teamsrsabs2)/(games/ofe))
 print(playervalue)
 print("if team srs is negative:")
-print(playervalue + teamsrsabs)
+print(-(playervalue + teamsrsabs))
 print("if team srs is positive:")
 print(teamsrsabs - playervalue)

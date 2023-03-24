803ccfda12d32f9f282f6e9762d5461cb0c9335e
Mason Seaquist <107698979+StrikeA@users.noreply.github.com>
2023-03-23 22:05:14 -0500

    stuff

--- final/Main_Trainer.py
+++ final/Main_Trainer.py
@@ -1,31 +1,27 @@
-import numpy as np
-import pandas as pd
-import sklearn
-import pickle
-from sklearn import linear_model
+import numpy as np                # Import numpy library for numerical operations
+import pandas as pd               # Import pandas library for data manipulation
+import sklearn                    # Import sklearn library for machine learning models
+import pickle                     # Import pickle library for saving models
+from sklearn import linear_model  # Import linear_model from sklearn
 
-data = pd.read_csv("Basket Ball Data.csv")
-# Use pandas to read the csv
-data = data[["HASRS", "AASRS", "HMOV"]]
-# Define columns being used
-predict = "HMOV"
-# defining predict as home margin of error
-X = np.array(data.drop([predict], axis=1))
-y = np.array(data[predict])
-best = 0
+data = pd.read_csv("Basket Ball Data.csv")   # Read the CSV file using pandas
+data = data[["HASRS", "AASRS", "HMOV"]]      # Define the columns to be used
+predict = "HMOV"                            # Define the dependent variable
+X = np.array(data.drop([predict], axis=1))  # Define the independent variables as a numpy array
+y = np.array(data[predict])                 # Define the dependent variable as a numpy array
+best = 0                                    # Initialize the best accuracy score to 0
+
 while best < 0.97:
+    # Split the data into training and testing sets
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.05)
-    # splitting data into train and test sets
-    linear = linear_model.LinearRegression()
-    # defining model
-    linear.fit(x_train, y_train)
-    acc = linear.score(x_test, y_test)
+    linear = linear_model.LinearRegression()  # Define the linear regression model
+    linear.fit(x_train, y_train)             # Fit the model to the training data
+    acc = linear.score(x_test, y_test)       # Calculate the accuracy score on the testing data
+
     if acc > best:
-        best = acc
+        best = acc                          # Update the best accuracy score
         print(best)
         with open("BasketBall_Model.pickle", "wb") as f:
-            pickle.dump(linear, f)
-print(best)
+            pickle.dump(linear, f)          # Save the model with the best accuracy score to a pickle file
 
-
-
+print(best)                                  # Print the final best accuracy score
--- final/main.py
+++ final/main.py
@@ -1,27 +1,38 @@
-import numpy as np
-import pickle
-import csv
+import numpy as np        # Import numpy library for numerical operations
+import pickle            # Import pickle library for loading saved models
+import csv               # Import csv library for writing data to a CSV file
 
-# Dictionary
+# Create a dictionary with keys for the home and away Adjusted SRS values and home margin of victory
 dict = {"HASRS": None, "AASRS": None, "HMOV": None}
 
+# Prompt the user to enter the home and away Adjusted SRS values and store them in the dictionary
 homeasrs = float(input("What is the home Adjusted SRS? "))
 awayasrs = float(input("What is the away Adjusted SRS? "))
-
 dict["HASRS"] = homeasrs
 dict["AASRS"] = awayasrs
+
+# Print the dictionary to the console
 print(dict)
-# list of column names
+
+# Create a list of column names for the CSV file
 field_names = ["HASRS", "AASRS", "HMOV"]
 
+# Open the CSV file in append mode and create a dictionary writer object
 with open("Basket Ball Data.csv", "a") as csv_file:
     dict_object = csv.DictWriter(csv_file, fieldnames=field_names)
 
+    # Write the dictionary to the CSV file
     dict_object.writerow(dict)
 
+# Load the saved linear regression model from the pickle file
 pickle_in = open("BasketBall_Model.pickle", "rb")
 linear = pickle.load(pickle_in)
+
+# Create a numpy array with the home and away Adjusted SRS values
 test_data = np.array([[homeasrs, awayasrs]])
+
+# Use the linear regression model to make a prediction and store the result in a variable
 prediction = linear.predict(test_data)
+
+# Print the prediction to the console
 print(prediction)
-#%%
